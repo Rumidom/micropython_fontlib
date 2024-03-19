51feed359b4ca7e9d213976dfa8aabb794c5e5fc
Zezo <jr.robertocv@gmail.com>
2024-03-19 01:08:16 -0300

    changing how invert works too also fill space between chars

--- fontlib.py
+++ fontlib.py
@@ -1,10 +1,10 @@
 import fonts_dictionary
 
-def printchar(letter,xpos,ypos,spce,fbuf,font_name = "five",invert = False,charwidth = 5):
+def printchar(letter,xpos,ypos,fbuf,font = "five",invert = False,charwidth = 5):
     origin = xpos
     charval = ord(letter)
     index = charval-32 #start code, 32 or space
-    character = fonts_dictionary.fontdict[font_name][index]
+    character = fonts_dictionary.fontdict[font][index]
     rows = [character[i:i+charwidth] for i in range(0,len(character),charwidth)]
     rowslen = len(rows)
     orig_y = ypos
@@ -16,18 +16,20 @@ def printchar(letter,xpos,ypos,spce,fbuf,font_name = "five",invert = False,charw
             if (invert == True): # inverts and adds padding
                 if (bit == '0'):
                     fbuf.pixel(xpos,ypos,1)
-                if (ypos == orig_y):
-                    fbuf.pixel(xpos,ypos-1,1)
-                if (ypos == orig_y+charwidth-1):
-                    fbuf.pixel(xpos,ypos+1,1)
             xpos+=1
         xpos=origin
         ypos+=1
 
-def printstring(string,xpos,ypos,spce,fbuf,font_name = "five",invert=False,charwidth=5):
-    orig_x = xpos
-    for i in string:
-        printchar(i,xpos,ypos,spce,fbuf,font_name = font_name,invert=invert,charwidth=charwidth)
-        xpos+=(spce+5)
+def printstring(string,xpos,ypos,spce,fbuf,font = "five",invert=False,charwidth=5):
+    character = fonts_dictionary.fontdict[font][0]
+    if invert:
+        string_width = (charwidth+spce)*(len(string)-1)+charwidth+1
+        string_height = len(character)//charwidth
+        fbuf.rect(xpos-1, ypos-1, string_width+1, string_height+2, 1)
+    for i,c in enumerate(string):
+        printchar(c,xpos,ypos,fbuf,font = font,invert=invert,charwidth=charwidth)
+        xpos+=(spce+charwidth)
+        if (invert and i < len(string)-1):
+            fbuf.rect(xpos-spce, ypos, spce, string_height, 1,1)
     
 
