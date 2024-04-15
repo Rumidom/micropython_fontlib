d95a4af428f8b7936a993f18486871655b37b11d
Zezo <jr.robertocv@gmail.com>
2024-04-15 16:06:40 -0300

    loads 1bit bitmaps directly

--- Examples/Nokia 5110_Example.py
+++ Examples/Nokia 5110_Example.py
@@ -11,9 +11,12 @@ lcd = pcd8544_fb.PCD8544_FB(spi, cs, dc, rst)
 buffer = bytearray((pcd8544_fb.HEIGHT // 8) * pcd8544_fb.WIDTH)
 fbuf = framebuf.FrameBuffer(buffer, pcd8544_fb.WIDTH, pcd8544_fb.HEIGHT, framebuf.MONO_VLSB)
 
+five = fontlib.font("five (5,5).bmp")
+futuristic = fontlib.font("futuristic (5,7).bmp")
+icons = fontlib.font("fonts/icons (5,7).bmp")
+
 fbuf.fill(0)
-font.printstring("The Quick Gray",0,0,1,fbuf,font_name = "cellphone")
-font.printstring("Fox Jumps Over",0,8,1,fbuf,font_name = "cellphone")
-font.printstring("The Lazy Dog",0,16,1,fbuf,font_name = "cellphone")
-  
+fontlib.prt("The Quick Gray",0,0,1,fbuf,five)
+fontlib.prt("Fox Jumps Over",0,8,1,fbuf,futuristic)
+fontlib.prt("The Lazy Dog",0,16,1,fbuf,icons)
 lcd.data(buffer)
--- fontlib.py
+++ fontlib.py
@@ -1,36 +1,124 @@
 import fonts_dictionary
+import math
 
-def DrawPixels(ypos,xpos,rows,fbuf,invert=False):
+def reverse_Bits(n, no_of_bits):
+    result = 0
+    for i in range(no_of_bits):
+        result <<= 1
+        result |= n & 1
+        n >>= 1
+    return result
+
+def getbitmap(path):
+    file = open(path, "rb")
+    filebytes = bytearray(file.read())
+    file.close()
+    imagesize = (int.from_bytes(filebytes[18:22],"little"),int.from_bytes(filebytes[22:26],"little"))
+    filezise =  int.from_bytes(filebytes[2:6],"little")
+    dataOffset = int.from_bytes(filebytes[10:14],"little")
+    format = int.from_bytes(filebytes[28:30],"little")
+    pallet = [0,1][b'\x00\x00\x00\xff\xff\xff\xff\xff' == filebytes[54:62]]
+    imagedata = bytearray(reversed(filebytes[dataOffset:]))
+    bytesperrow = math.ceil(imagesize[0]/8)
+    rowPadding = ((4-(bytesperrow%4))%4)
+    rowsize = bytesperrow +rowPadding
+    if pallet:
+        for i,byte_ in enumerate(imagedata):
+            imagedata[i] = byte_^ 0xff
+    imagerows = []
+    for i,byte_ in enumerate(imagedata):
+        imagedata[i] = reverse_Bits(byte_,8)
+    for i in range(imagesize[1]):
+        imagerows.append(bytearray(reversed(imagedata[i*rowsize:(i+1)*rowsize])))
+    imagerows = tuple(imagerows)
+    return (imagerows,imagesize,filezise,dataOffset,format)
+
+def getCutTile(imagerows,tilepos,chopsize,imagesize,paddingsize = 1):
+    bytesperrow = math.ceil(imagesize[0]/8)
+    rowPadding = ((4-(bytesperrow%4))%4)
+    rowsize = bytesperrow +rowPadding
+    bitsperrow = bytesperrow*8
+    bitpadding = bitsperrow-imagesize[0]
+    newchopsize = (chopsize[0]+paddingsize*2,chopsize[1]+paddingsize*2)
+
+    left = tilepos[0] * newchopsize[0]
+    upper = tilepos[1] * newchopsize[1]
+    cutdata = bytearray()
+    k = 0
+    b = 0x00
+    for j in range(paddingsize,newchopsize[1]-paddingsize,1):
+        for i in range(paddingsize,newchopsize[0]-paddingsize,1):
+            bitindex = left+i
+            currentbyte = imagerows[j+upper][bitindex//8]
+            bitpos =  bitindex%8
+    
+            if ((currentbyte >> bitpos) & 1):
+                b |= ((1 << k))
+            else:
+                b &= ((1 << k) ^ 0xFF)
+            k +=1
+            if k > 7 or ((j == newchopsize[1]-paddingsize-1) and (i == newchopsize[0]-paddingsize-1)):
+                cutdata.append(b)
+                b = 0x00
+                k = 0
+    return cutdata
+
+def chop_image(imagepath, chopsize,paddingsize = 1):
+    #like dicing an onion
+    bmptup = getbitmap(imagepath)
+    width,height = bmptup[1]
+    num_rows = height // (chopsize[1]+paddingsize*2)
+    num_cols = width // (chopsize[0]+paddingsize*2)
+
+    cut_images = []
+    for y in range(num_rows):
+        for x in range(num_cols):
+            cutdata = getCutTile(bmptup[0],(x,y),chopsize,bmptup[1],paddingsize = paddingsize)
+            cut_images.append(cutdata)
+    
+    return cut_images
+
+class font():
+    def __init__(self,bmp_path):
+        try:
+            bmpname = bmp_path.split("/")[-1]
+            szstrlist = bmpname.split(" (")[1].split(").")[0].split(",")
+        except:
+            raise Exception("invalid fontpath, check if file exist and fontname includes fontsize")
+
+        self.size = (int(szstrlist[0]),int(szstrlist[1]))
+        self.CharList = chop_image(bmp_path, self.size,paddingsize = 1)
+    
+    def getchar(self,N):
+        return self.CharList[N]
+    
+def DrawPixels(xpos,ypos,charbytes,charsize,fbuf,invert=False):
     orig_y = ypos
     orig_x = xpos
-    for row in rows:
-        for bit in row:
-            if ((bit == '1') and (invert == False)):
-                fbuf.pixel(xpos,ypos,1)
-            if (invert == True): # inverts and adds padding
-                if (bit == '0'):
-                    fbuf.pixel(xpos,ypos,1)
-            xpos+=1
-        xpos=orig_x
-        ypos+=1
-        
-def printchar(letter,xpos,ypos,fbuf,font = "five",invert = False,charwidth=None):
+    for byt in charbytes:
+        for i in range(8):
+            if ypos<charsize[1]+orig_y:
+                fbuf.pixel(xpos,ypos,(invert,not invert)[((byt >> i) & 1)])
+            xpos+= 1
+            if xpos >= charsize[0]+orig_x:
+                xpos = orig_x
+                ypos += 1
+            
+def printchar(letter,xpos,ypos,fbuf,font,invert = False,charwidth=None):
     if charwidth == None:
-        charwidth = fonts_dictionary.fontdict['fontsize'][font][0]
+        charwidth = font.size[0]
+    origin = xpos
+    
     charval = ord(letter)
+    if charval > 127: #Todo support for portuguese special characters
+        charval = 0x3f
     index = charval-32 #start code, 32 or space
-    try:
-        character = fonts_dictionary.fontdict[font][index]
-    except Exception as e:
-        print("Character N:",charval)
-        print(e)
-        
-    rows = [character[i:i+charwidth] for i in range(0,len(character),charwidth)]
-    rowslen = len(rows)
-    DrawPixels(ypos,xpos,rows,fbuf,invert=invert)
+    character = font.getchar(index)
 
-def printstring(text,xpos,ypos,spce,fbuf,font = "five",invert=False):
-    char_size = fonts_dictionary.fontdict['fontsize'][font]
+    DrawPixels(xpos,ypos,character,font.size,fbuf,invert=invert)
+
+def prt(string,xpos,ypos,spce,fbuf,font,invert=False):
+    char_size = font.size
     if invert:
         string_width = (char_size[0]+spce)*(len(string)-1)+char_size[0]+1
         string_height = char_size[1]
